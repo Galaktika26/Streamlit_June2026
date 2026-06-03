516562b4e52f91c9df210c9b4852557fe21be530
Galaktika26 <shayan2k8@gmail.com>
2026-06-03 16:59:38 -0400

    Update finanalysis.py

--- finanalysis.py
+++ finanalysis.py
@@ -31,7 +31,7 @@ def portfolio_optimization(returns):
 st.title("Financial Analysis Web App")
 
 # User inputs
-tickers = st.text_input("Enter stock tickers (comma separated)", "AAPL, MSFT, GOOG, NVIDIA").split(",")
+tickers = st.text_input("Enter stock tickers (comma separated)", "AAPL, MSFT, GOOG, NVDA").split(",")
 tickers = [ticker.strip() for ticker in tickers]
 start_date = st.date_input("Start Date", pd.to_datetime("2020-01-01"))
 end_date = st.date_input("End Date", pd.to_datetime("2023-01-01"))
