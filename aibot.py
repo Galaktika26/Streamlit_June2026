a2d8091295680c2da738e4a206c7113ccc9f1c4f
Galaktika26 <shayan2k8@gmail.com>
2026-06-03 16:01:29 -0400

    Update aibot.py

--- aibot.py
+++ aibot.py
@@ -8,7 +8,7 @@ import json
 def response_generator():
     response = ai_ask("Pretend you are a very friendly and helpful person.  Please provide a response given the provided context. Please provide the   response only with no before or after commentary.",
     data=st.session_state.messages,
-    api_key=st.secrets["Ig28DC3zkACpVBzBF1lEbCUKjhPRPN3p"])
+    api_key=st.secrets["api_key"])
     for word in response.split():
         yield word + " "
         time.sleep(0.05)
